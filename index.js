c8d31c6b01674b5f33a03179e851ea96b7278892
Aaron <aaronmcavinue@gmail.com>
2016-11-18 22:00:54 +0000

    Add user auth and password hashing.

--- index.js
+++ index.js
@@ -2,10 +2,52 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var User = require('./user-model');
+var bcrypt = require('bcryptjs');
+var passport = require('passport');
+var BasicStrategy = require('passport-http').BasicStrategy;
+
 
 var app = express();
 
 var jsonParser = bodyParser.json();
+
+var strategy = new BasicStrategy(function(username, password, callback) {
+    User.findOne({
+        username: username
+    }, function (err, user) {
+        if (err) {
+            callback(err);
+            return;
+        }
+
+        if (!user) {
+            return callback(null, false, {
+                message: 'Incorrect username.'
+            });
+        }
+
+        user.validatePassword(password, function(err, isValid) {
+            if (err) {
+                return callback(err);
+            }
+
+            if (!isValid) {
+                return callback(null, false, {
+                    message: 'Incorrect password.'
+                });
+            }
+            return callback(null, user);
+        });
+    });
+});
+
+passport.use(strategy);
+
+app.get('/hidden', passport.authenticate('basic', {session: false}), function(req, res) {
+    res.json({
+        message: 'Luke... I am your father'
+    });
+});
 
 app.post('/users', jsonParser, function(req, res) {
     if (!req.body) {
@@ -58,19 +100,35 @@ app.post('/users', jsonParser, function(req, res) {
         });
     }
 
-    var user = new User({
-        username: username,
-        password: password
-    });
-
-    user.save(function(err) {
+    bcrypt.genSalt(10, function(err, salt) {
         if (err) {
             return res.status(500).json({
-                message: 'Internal server error'
+                message: 'Internal server error 1'
             });
         }
 
-        return res.status(201).json({});
+        bcrypt.hash(password, salt, function(err, hash) {
+            if (err) {
+                return res.status(500).json({
+                    message: 'Internal server error 2'
+                });
+            }
+
+            var user = new User({
+                username: username,
+                password: hash
+            });
+
+            user.save(function(err) {
+                if (err) {
+                    return res.status(500).json({
+                        message: 'Internal server error 3'
+                    });
+                }
+
+                return res.status(201).json({});
+            });
+        });
     });
 });
 
